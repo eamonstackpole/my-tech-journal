69ad17debfdd018dbaf0e14c87b3a38305f06693
eamonstackpole <eamon.stackpole@mymail.champlain.edu>
2024-02-27 23:55:55 +0000

    updated ssh script

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -3,4 +3,16 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+
+#Creates the User
+sudo useradd -m -d /home/$1 -s /bin/bash $1
+sudo mkdir /home/$1/.ssh
+sudo cp /home/eamon/my-tech-journal/SYS265/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+sudo chmod 700 /home/$1/.ssh
+sudo chmod 600 /home/$1/.ssh/authorized_keys
+sudo chown -R $1:$1 /home/$1/.ssh
+
+#Disables Root
+sudo sed -n  '33 s/yes/no/' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+
